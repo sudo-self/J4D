e5679606d133676afb13c41c4c1e0ffcec76e12d
sudo-self <sudo-self@proton.me>
2025-09-26 08:32:46 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Jesse 3D",
   description: "Jesse 3D with Three JS",
+  openGraph: {
+    url: "https://Jesse3d.pages.dev",
+    title: "Jesse 3D",
+    description: "Explore 3D models in Three.js with Jesse3D",
+    siteName: "Jesse 3D",
+    images: [
+      {
+        url: "/J3d.png",
+        width: 1200,
+        height: 630,
+        alt: "Jesse 3D Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jesse 3D",
+    description: "Explore 3D models in Three.js with Jesse3D",
+    images: ["/J3d.png"],
+  },
 };
 
 export default function RootLayout({
@@ -28,9 +48,19 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192-maskable.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icon-192-maskable.png"
+        />
         <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512-maskable.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icon-512-maskable.png"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
@@ -38,11 +68,12 @@ export default function RootLayout({
         {children}
 
         <footer className="fixed bottom-4 left-0 w-full text-center text-white hover:text-yellow-400 transition-colors duration-300 cursor-pointer z-50">
-        ᴶᵉˢˢᵉ³ᵈ @𝕣𝕖𝕒𝕔𝕥-𝕥𝕙𝕣𝕖𝕖/𝕗𝕚𝕓𝕖𝕣
+          ᴶᵉˢˢᵉ³ᵈ @𝕣𝕖𝕒𝕔𝕥-𝕥𝕙𝕣𝕖𝕖/𝕗𝕚𝕓𝕖𝕣
         </footer>
       </body>
     </html>
   );
 }
+
 
 
