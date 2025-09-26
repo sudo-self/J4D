6248eb9bc5523f60759ee3bc993c013b8d0f07c7
sudo-self <sudo-self@proton.me>
2025-09-26 12:29:26 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,19 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] })
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Jesse 3D",
-  description: "Jesse 3D with Three JS",
+  title: "Jesse 4D",
+  description: "Jesse 4D with Three.js",
   openGraph: {
-    url: "https://Jesse3d.pages.dev",
-    title: "Jesse 3D",
-    description: "Explore 3D models in Three.js with Jesse3D",
-    siteName: "Jesse 3D",
-    images: [{ url: "/J3d.png", width: 1200, height: 630, alt: "Jesse 3D Preview" }],
+    url: "https://j4d.jessejesse.com",
+    title: "J4d",
+    description: "Explore 3D models in Three.js with Jesse 4D",
+    siteName: "Jesse 4D",
+    images: [{ url: "/J3d.png", width: 1200, height: 630, alt: "Jesse 4D Preview" }],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Jesse 3D",
-    description: "Explore 3D models in Three.js with Jesse3D",
+    title: "Jesse 4D",
+    description: "Explore 3D models in Three.js with Jesse 4D",
     images: ["/J3d.png"],
   },
 }
@@ -28,11 +28,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icon-192-maskable.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/icon-512-maskable.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
@@ -42,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
+
 
 
 
